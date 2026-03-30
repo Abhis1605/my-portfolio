d90782ecc104ee6ee8e3f179527a4d67fb865563
Abhi solanki <abhi85076@gmail.com>
2026-03-30 23:05:45 +0530

    fix

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+        <div className='px-[8%] lg:px-[5%] hero-header' id='home'>
+
+            <div className='hero-content w-full lg:w-1'>
+
+            </div>
+
+
+        </div>
+    </>
+  )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { Download, Menu } from "lucide-react"
+import { Download, Menu, MoveUpRight } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
@@ -25,22 +25,22 @@ export default function Navbar() {
 
         <div className={`w-full flex items-center justify-between transition-all duration-500 ${isFixed ? "px-[8%] lg:px-[16%]" : "px-[8%] lg:px-[5%]"}`}>
 
-            <Link href="/" className={`text-3xl lg:text-4xl font-bold text-white`}>
-                Abhi<span>.dev</span>
+            <Link href="/" className="text-2xl md:text-3xl lg:text-3xl font-semibold text-white font-display">
+                Abhi<span className="font-display  text-(--color-surface-2)">.dev</span>
             </Link>
 
             <nav className="hidden lg:flex space-x-5 menu-link relative z-40">
 
                 {navLinks.map((link) => (
-                    <Link key={link.label} href={link.href} className="text-white hover:text-blue-600 transition-all duration-300">
+                    <Link key={link.label} href={link.href} className="font-display tracking-widest text-white hover:text-blue-600  font-bold transition-all duration-300">
                         {link.label}
                     </Link>
                 ))}
             </nav>
 
             <div className="flex items-center gap-4">
-                <Link className="flex items-center gap-2 px-4 py-2 bg-amber-500 rounded-full text-sm" href="/">
-                    <Download className="w-5 h-5" />
+                <Link className="flex items-center gap-2 px-5 py-1 text-sm bg-transparent text-white border-2 hover:bg-white font-display tracking-widest hover:text-accent transition-all duration-300 hover:border-none hover:scale-95" href="/">
+                    <MoveUpRight className="w-4 h-4 " />
                     Resume
                 </Link>
             </div>
