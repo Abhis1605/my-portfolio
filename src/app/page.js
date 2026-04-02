a871dc8319a9e3bf7bb99b088f7f0e2d2938839e
Abhi solanki <abhi85076@gmail.com>
2026-04-02 12:13:24 +0530

    feat: home projects

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Timeline from "@/components/Timeline";
 
@@ -13,6 +14,7 @@ export default function Home() {
       <About />
       <Timeline />
       <Skills />
+      <Projects  />
     </div>
   );
 }
---
+++ src/components/common/ProjectCard.jsx
@@ -0,0 +1,146 @@
+import { Move, MoveUpRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const projects = [
+  {
+    id: 1,
+    title: "DogVerse",
+    desc: "React based website that displays dog breeds using Dog CEO API with search and category filtering",
+    image: "/projects/dogverse.webp",
+    tech: ["React", "CSS", "React Router"],
+    category: "frontend",
+    live: "https://dogverse.vercel.app",
+    github: "https://github.com/Abhis1605/dogverse",
+    learned: [
+      "Fetching data from APIs",
+      "Implementing dynamic routing using React Router",
+      "Building search and category filtering",
+      "Creating Responsive UI"
+    ]
+  }
+  ,
+  {
+    id: 2,
+    title: "NoteStack",
+    desc: "Full-Stack MERN notes app with authentication, CRUD operations, and search & pin features.",
+    image: "/projects/note-stack.png",
+    tech: ["React", "Node.js","Express", "MongoDB", "Tailwind"],
+    category: "frontend",
+    live: "https://dogverse.vercel.app",
+    github: "https://github.com/Abhis1605/dogverse",
+    learned: [
+      "Building REST APIs",
+      "Implementing authentication (login/signup)",
+      "Managing CRUD operations",
+      "Adding features like search and pin notes",
+      "Handling frontend-backend integration"
+    ]
+  },
+  {
+    id: 3,
+    title: "EduNest",
+    desc: "Student management portal built as a final year project using Next.js and modern tools.",
+    image: "/projects/edunest.webp",
+    tech: ["Next.js", "NextAuth", "MongoDB", "Tailwind", "ShadCN"],
+    category: "fullstack",
+    live: "#",
+    github: "#",
+    featured: true,
+    learned: [
+      "Understanding Next.js architecture",
+      "Building APIs in Next.js",
+      "Implementing authentication using NextAuth",
+      "Using Context API for state management",
+      "Working with modern UI libraries like ShadCN"
+    ]
+  },
+    {
+    id: 4,
+    title: "Elite Estate",
+    desc: "Frontend real estate UI built using React and Tailwind with clean and modern design.",
+    image: "/projects/elite-e-state.webp",
+    tech: ["React", "Tailwind"],
+    category: "frontend",
+    live: "https://eliteestat.netlify.app",
+    github: "#",
+    featured: false,
+    learned: [
+      "Designing modern UI with Tailwind",
+      "Component-based architecture in React",
+      "Improving layout and responsiveness"
+    ]
+  },
+
+  {
+    id: 5,
+    title: "Local Mart",
+    desc: "E-commerce website built using HTML, CSS, and JavaScript with cart functionality.",
+    image: "/projects/local-mart.webp",
+    tech: ["HTML", "CSS", "JavaScript"],
+    category: "frontend",
+    live: "https://local-mart-kappa.vercel.app",
+    github: "#",
+    featured: false,
+    learned: [
+      "Working with pure JavaScript logic",
+      "Implementing add to cart functionality",
+      "Understanding basic e-commerce flow",
+      "Handling DOM manipulation"
+    ]
+  },
+]
+
+export default function ProjectCard() {
+  return (
+   <>
+    {projects.map((project, index) => (
+      <div key={index} className='flex flex-col bg-[#141414] border-2 border-border rounded-md p-4'>
+
+        <div className='border-4 border-border rounded-md mb-3 overflow-hidden'>
+          <div className=' relative w-full h-50' >
+          <Image className='object-cover' src={project.image} alt='project-image'fill  />
+        </div>
+        </div>
+
+        <div>
+          <h4 className='text-xl tracking-wider mb-1 font-extrabold'>
+            {project.title}
+          </h4>
+          <p className='text-sm
+           text-justify text-muted font-semibold mb-2'>
+            {project.desc}
+          </p>
+          <div className='flex gap-2 mb-4 flex-wrap'>
+            {project.tech.map((tag,index) => (
+              <div className='bg-accent-muted px-2 py-1 rounded-full' key={index}>
+                <p className='font-display text-xs text-accent-warm'>{tag}</p>
+              </div>
+            ))}
+          </div>
+          <div className='flex gap-3 '>
+            <Link target='_blank' className='flex px-3 py-2 bg-accent text-sm items-center justify-center gap-1 rounded-full hover:bg-white hover:text-accent transition-colors duration-300' href={project.live}>
+            Live <MoveUpRight className='w-3 h-3' />
+          </Link>
+          <Link target='_blank' className='flex px-3 py-2 bg-accent text-sm items-center justify-center gap-1 rounded-full hover:bg-white hover:text-accent transition-colors duration-300' href={project.github}>
+            GitHub <MoveUpRight className='w-3 h-3' />
+          </Link>
+          </div>
+        </div>
+      <div className='mt-2'>
+        <p className='text-xs text-muted'>What I learned:</p>
+        <ol className='list-disc text-xs text-muted pl-4'>
+            {project.learned.map((item, index) => (
+              <li key={index}>
+                {item}
+              </li>
+            ))}
+        </ol>
+      </div>
+      
+    </div>
+    ))}
+   </>
+  )
+}
